544a2787655b107868e7a948eeeeb5b06ebb51e8
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-06-24 18:47:27 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily : {
+          sans:['vazir'],
+        },
+
+      },
+    },
+    plugins: [
+      require("@tailwindcss/forms")({
+        strategy:"class",
+      }),
+    ],
+  }
